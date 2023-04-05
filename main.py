2a3f7e019f42ae770b836485cff59d09699bb409
Prashantkhobragade <prashantkhobragade011@gmail.com>
2023-04-06 01:58:00 +0530

    data ingestion need to check

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
 from sensor.logger import logging
 import sys
+from sensor.pipeline.training_pipeline import TrainPipeline
 
-def test_exception():
-    try:
-        logging.info("dividing 1 by 0")
-        x=1/0
-    except Exception as e:
-        raise SensorException(e,sys)
+
 
 if __name__ == '__main__':
-    try:
-        test_exception()
-    except Exception as e:
-        print(e)
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
     
-    #mongodb_client = MongoDBClient()
+  
